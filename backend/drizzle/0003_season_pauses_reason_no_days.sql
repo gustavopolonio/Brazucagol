260289c13b86323132184a43e3394cf7bdc5b42b
Gustavo Polonio <gustavopolonio1@gmail.com>
2026-01-13 09:30:53 -0300

    resetting all migrations

--- backend/drizzle/0003_season_pauses_reason_no_days.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "season_pauses" DROP COLUMN "days";
---> statement-breakpoint
-ALTER TABLE "season_pauses" ADD COLUMN "reason" text;
-
--- backend/drizzle/0004_milky_mesmero.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "player_round_stats" ADD COLUMN "auto_goal_attempts" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "player_round_stats" ADD COLUMN "penalty_attempts" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "player_round_stats" ADD COLUMN "free_kick_attempts" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "player_round_stats" ADD COLUMN "trail_attempts" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "player_total_stats" ADD COLUMN "auto_goal_attempts" integer DEFAULT 0 NOT NULL;
